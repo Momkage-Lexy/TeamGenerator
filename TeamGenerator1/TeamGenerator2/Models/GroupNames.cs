a7e314dcc7be698671488f19befbd04473ef5c52
Momkage-Lexy <lexy.b.quijano@gmail.com>
2024-10-03 21:08:22 -0700

    Set up model for form

---
+++ TeamGenerator1/TeamGenerator2/Models/GroupNames.cs
@@ -0,0 +1,10 @@
+using System;
+using System.IO;  
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+namespace Homework1.Models;
+
+public class GroupNames
+{
+    //ToDo
+}
