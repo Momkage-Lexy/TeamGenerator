41e3ed85a7d9e48fcb5ad303d5f224152a392564
Momkage-Lexy <lexy.b.quijano@gmail.com>
2024-10-04 18:15:20 -0700

    View loops for team size and creates a section each time

--- TeamGenerator1/TeamGenerator2/obj/Debug/net8.0/TeamGenerator2.AssemblyInfo.cs
+++ TeamGenerator1/TeamGenerator2/obj/Debug/net8.0/TeamGenerator2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamGenerator2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d9b1839fc96c8f019880546fb4e9f4efb41ae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a62890eda5d6b88f9aa395dc76363e5a566e5c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamGenerator2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamGenerator2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
